484901a94c992ef2bf8e83c43c4735fb5e63de0b
José Manuel Lucas <josemanuel@jmlweb.es>
2023-12-05 21:26:11 +0100

    chore: enable coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
