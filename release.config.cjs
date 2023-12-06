ac7e3960a1b78731c18b2593ad397f498a8b5d1b
José Manuel Lucas <josemanuel@jmlweb.es>
2023-12-06 02:11:43 +0100

    fix: plugins for semantic release

--- release.config.cjs
+++ release.config.cjs
@@ -5,12 +5,10 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      '@semantic-release/git',
-    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git',
   ],
 };
