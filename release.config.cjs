286a670eeb6d517aea117b88eb66cad70930efc2
José Manuel Lucas <josemanuel@jmlweb.es>
2026-01-27 15:12:42 +0100

    ci: use oidc for npm publish without token (#13)

--- release.config.cjs
+++ release.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
+    ['@semantic-release/npm', { npmPublish: false }],
     '@semantic-release/github',
     '@semantic-release/git',
   ],
